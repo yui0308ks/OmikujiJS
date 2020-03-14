9f866008df57eb960eaab5cd1d4405c5520c2c66
yui0308ks <kazuyu03081@gmail.com>
2020-03-14 15:34:51 +0900

    配列を使用

--- js/main.js
+++ js/main.js
@@ -2,21 +2,22 @@
 
 {
   const btn = document.getElementById('btn');
-
+  const results = ['大吉', '中吉', '凶', '末吉', '小吉'];
   btn.addEventListener('click',() => {
-    const n = Math.floor(Math.random() * 3);
-    // btn.textContent = n;
+    // const n = Math.floor(Math.random() * results.length);
+    // btn.textContent = results[n];
+  btn.textContent = results[Math.floor(Math.random() * results.length)];
 
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
-    }
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     btn.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     btn.textContent = '凶';
+    //     break;
+    // }
   });
 }
